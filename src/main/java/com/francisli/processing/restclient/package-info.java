baf6c98f84495f3199df9d707fea8bdadd8d33a8
Francis Li <mail@francisli.com>
2017-09-03 21:51:47 -0700

    Fix links in docs

--- src/main/java/com/francisli/processing/restclient/package-info.java
+++ src/main/java/com/francisli/processing/restclient/package-info.java
@@ -2,12 +2,12 @@
  * <p>This Processing REST Client library can be used to communicate with servers that expose REST (REpresentational
  * State Transfer) style APIs (Application Programming Interfaces) over the HTTP protocol.</p>
  * <p>To get started, refer to the RestClient documentation.</p>
- * <p><a href="https://github.com/downloads/francisli/processing-http/http.zip">Download now from Github (restclient.zip)</a></p>
- * <p><a href="https://github.com/francisli/processing-http/wiki">See (and contribute!) examples on the wiki</a></p>
- * 
+ * <p><a href="https://github.com/francisli/processing-restclient/releases/latest">Download now from Github (restclient.zip)</a></p>
+ * <p><a href="https://github.com/francisli/processing-restclient/wiki">See (and contribute!) examples on the wiki</a></p>
+ *
  * @name RESTClient
  * @author Francis Li
- * 
+ *
  * @class RESTClient
  * @class HttpRequest
  * @class HttpResponse
